22913380f5fd91913fcfea2ed70b4255d7559aaf
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2023-06-03 19:00:18 -0400

    Componentes en React

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Header from "./components/Header";
 function App() {
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-      <h1 className="text-4xl font-extrabold text-gray-500 text-center">
-        Cuanto <span className="text-indigo-600">dinero </span> necesitas?
-      </h1>
+      <Header />
+      <input
+        type="range"
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+      />
     </div>
   );
 }
