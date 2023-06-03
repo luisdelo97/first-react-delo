d8bc66dc79c36479f7d514e3b13ba75413ba8629
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2023-06-03 18:06:19 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <h1 className="text-4xl font-extrabold text-gray-500 text-center">
+        Cuanto <span className="text-indigo-600">dinero </span> necesitas?
+      </h1>
+    </div>
+  );
+}
+
+export default App;
