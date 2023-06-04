59c0ab869f2999f542eeb0fc10e8ac70bb13ff02
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2023-06-04 00:32:55 -0400

    Aprendiendo aa usar useEffect y realizando el deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Button from "./components/Button";
-import { formatearDinero } from "./helpers";
+import { formatearDinero, calcularTotalPagar } from "./helpers";
+
 function App() {
   const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6);
+  const [total, setTotal] = useState(0);
+  const [pago, setPago] = useState(0);
+
+  useEffect(() => {
+    const resultadoTotalPagar = calcularTotalPagar(cantidad, meses);
+    setTotal(resultadoTotalPagar);
+
+    //calcular pago
+    setPago(total / meses);
+  }, [cantidad, meses, total]);
 
   const MIN = 0;
   const MAX = 20000;
@@ -50,6 +62,32 @@ function App() {
       <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
         {formatearDinero(cantidad)}
       </p>
+      <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+        Elige un <span className="text-indigo-600">Plazo </span>a pagar:
+      </h2>
+      <select
+        className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500"
+        value={meses}
+        onChange={(e) => setMeses(+e.target.value)}
+      >
+        <option value="6">6 Meses</option>
+        <option value="12">12 Meses</option>
+        <option value="24">24 Meses</option>
+      </select>
+      <div className="my-5 space-y-3 bg-gray-50 p-5">
+        <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+          Resumen <span className="text-indigo-600">de pagos </span>
+        </h2>
+        <p className="text-xl text-gray-500 text-center font-bold">
+          {meses} Meses
+        </p>
+        <p className="text-xl text-gray-500 text-center font-bold">
+          {formatearDinero(total)} Total a pagar
+        </p>
+        <p className="text-xl text-gray-500 text-center font-bold">
+          {formatearDinero(pago)} Mensuales
+        </p>
+      </div>
     </div>
   );
 }
